228bf7f30e89ecad91dcc4dc11a5b313a07b1497
VajihuddinAhmed <vajihahmed@gmail.com>
2021-05-03 22:38:15 +0200

    Project completed and completed converting our web application into responsive application.

--- src/components/read-section/read-section.js
+++ src/components/read-section/read-section.js
@@ -7,7 +7,7 @@ const ReadSection = () => (
     <div className="section-container">
         <div className="section-preview">
             <h1 className="title">Read Posts</h1>
-            <p className="para">To put it simply, BeFunky makes photo editing easy. With our <br/> world famous Photo Editor, you can turn photos you like into <br/> photos you love! From essential editing tools such as crop, <br/> resize, and exposure to our more unique effects like <br/> Cartoonizer, Digital Art, and Enhance DLX, it's beyond easy to <br/> create great looking photos. Add a little extra flair to your <br/> image with hundreds of customizable vector icons and graphic <br/> overlays. If picture quotes are your thing, BeFunky's Photo <br/> Editor has hundreds of free fonts for you to choose from. <br/> Finally, a photo editor that's powerful, fun, AND easy to use!</p>
+            <p className="para">Reading is one of the good habits we can cultivate in ourselves <br/> from our childhood, Reading indeed helps us score well in academics.<br/> But the benefits of having good reading habit is not limited to institutional <br/> education and purpose of knowledge.<br/> Reading ensures our mental wellness, boosts our confidence  <br/> and enriches our knowledge. A News Post is beyond easy to <br/> write and read great looking articles. Add a little extra flair to your <br/> articles with hundreds of customizable vector icons and graphic <br/> overlays. One, who loves to read, is addicted to  <br/> reading because of the pleasure you will gets from it. <br/> Finally, a News Post website is powerful, fun, AND easy to use!</p>
             <a href="#read-posts">
                 <CustomButton>Read Posts</CustomButton>
             </a>
